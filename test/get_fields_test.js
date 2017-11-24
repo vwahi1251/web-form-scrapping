d53a4f8c11396ddc377ab2f6882f9689d5f30733
stu <stu@fillr.com>
2017-11-24 12:29:22 +1100

    senior test initial commit

---
+++ test/get_fields_test.js
@@ -0,0 +1,28 @@
+const expect = chai.expect;
+
+expectation = [
+  { "address_line_1" : "Address line 1" },
+  { "cc_number" : "Number" },
+  { "cc_type" : "Type" },
+  { "country" : "Country" },
+  { "first_name" : "First Name" },
+  { "last_name" : "Last Name" }
+];
+
+describe('Widget #getFields', () => {
+
+  before((done) => {
+    fixture.setBase('assets');
+    fixture.load('top.html');
+
+    document.addEventListener('frames:loaded', function (event) {
+      this.result = window[event.detail.widget].getFields();  
+      done();  
+    }.bind(this));
+  });
+
+  it('should extract the fields', () => {
+    console.log(this.result);
+    expect(this.result).to.deep.equal(expectation);
+  });
+});
---
+++ widget/widget.js
@@ -0,0 +1,3 @@
+// Write your module here
+// It must attach an object to the window that exposes a function getFields(). 
+// getFields() should return a list of  { name:label } pairs describing all the fields in each frame
